a6d28d7025b92f8e807fae6ab07153b978f00d15
Eugene Rudoi <magl88@users.noreply.github.com>
2023-05-15 17:43:27 +0200

    add logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,17 @@
+import cn from 'classnames';
+import styles from './Logo.module.scss';
+import { LogoProps } from './Logo.props';
+import Link from 'next/link';
+import Logo2 from '@/assets/icons/logo2.svg';
+
+export const Logo = ({ children, className, ...props }: LogoProps): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <Link className={cn(className, styles.logo)} href={'/'}>
+        <Logo2 />
+        <span className={styles.span1}>EDM</span>
+        <span className={styles.span2}>Box</span>
+      </Link>
+    </div>
+  );
+};
