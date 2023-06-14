93ebd203b21b7e025c975e958ee70dab7db790ad
Eugene Rudoi <magl88@users.noreply.github.com>
2023-06-14 23:26:47 +0200

    add menu interface

---
+++ src/types/menu.interface.ts
@@ -0,0 +1,21 @@
+export interface FirstLevelMenuItem {
+  route: FirstLevelMenuRoute;
+  // name: FirstLevelMenuName;
+  secondLevel?: [SecondLevelMenuItem];
+}
+
+export interface SecondLevelMenuItem {
+  route: string;
+  // name: string;
+  parent: FirstLevelMenuRoute;
+}
+
+export enum FirstLevelMenuRoute {
+  'home',
+  'events',
+  'projects',
+  'locations',
+  'peoples',
+  'blog',
+  'edm-box',
+}
